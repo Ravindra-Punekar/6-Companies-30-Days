7fda60040202ac1bd4bd3899d9bc0e1e8f042208
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-05 00:51:48 +0530

    Solved

--- Challenge Company 1 : Microsoft/13. Seat Arrangement in a SpiceJet.cpp
+++ Challenge Company 1 : Microsoft/13. Seat Arrangement in a SpiceJet.cpp
@@ -1,3 +1,14 @@
 /*
 https://leetcode.com/problems/airplane-seat-assignment-probability/
 */
+
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+       if(n==1) return double(1);
+       else return double(0.5);
+        
+        //either passenger has picked nth seat or another seat
+        
+    }
+};
