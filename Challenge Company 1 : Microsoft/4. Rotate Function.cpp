b941180b499ae8a33e776c2f7ad2f8741eeef1e0
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-05 22:51:28 +0530

    Solved

--- Challenge Company 1 : Microsoft/4. Rotate Function.cpp
+++ Challenge Company 1 : Microsoft/4. Rotate Function.cpp
@@ -1,3 +1,26 @@
 /*
 https://leetcode.com/problems/rotate-function/
 */
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int maxVal =0;
+        int sum=0;
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+            maxVal += i*nums[i];
+        }
+
+        int prev = maxVal;
+       
+        for(int i=0; i<n; i++){
+            prev = prev + sum - n*nums[n-i-1];
+            maxVal = max(maxVal,prev);
+        }
+
+        return maxVal;
+    }
+};
