17db80683155717600c14e0c51f64adf8a608c9e
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-10 02:22:45 +0530

    Solved

--- Challenge Company 1 : Microsoft/3. Bulls and Cow.cpp
+++ Challenge Company 1 : Microsoft/3. Bulls and Cow.cpp
@@ -1,3 +1,32 @@
 /*
 https://leetcode.com/problems/bulls-and-cows/
 */
+
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        string ans;
+        int n = secret.size();
+        int b=0,c=0;
+        
+        vector<int> v1(10,0);
+        vector<int> v2(10,0);
+        
+        for(int i=0;i<n;i++){
+            if(secret[i]==guess[i]){
+                b++;
+            }else{
+                v1[(secret[i]-'0')]++;
+                v2[(guess[i]-'0')]++;
+            }
+        }
+
+        for(int i=0;i<10;i++){
+           c += min(v1[i],v2[i]);
+        }
+
+        ans = (to_string(b) + "A" + to_string(c) + "B");
+        
+        return ans;
+    }
+};
