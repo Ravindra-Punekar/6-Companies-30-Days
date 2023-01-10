fa6a8f00525604c5b6a23e9396aba102f1350e90
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-10 14:34:59 +0530

    Solved

--- Challenge Company 2 : Goldman Sachs/14. Nice Pairs in Array.cpp
+++ Challenge Company 2 : Goldman Sachs/14. Nice Pairs in Array.cpp
@@ -1,3 +1,44 @@
 /*
 https://leetcode.com/problems/count-nice-pairs-in-an-array/
 */
+
+class Solution {
+public:
+   
+    int rev(int n){
+        int x=0;
+        while(n>0){
+            x += n%10;
+            if(n>=10)    x*=10;
+            n /=10;
+        }
+        return x;
+    }
+
+    int countNicePairs(vector<int>& nums) {
+        /*      a + rev(b) = rev(a) + b 
+            =>  a - rev(a) = b - rev(b)         */
+        
+        int MOD = 1e9+7;
+        int count=0;
+        int n = nums.size();
+        
+        unordered_map<int,int> mp;
+        vector<int> diff; 
+        
+        for(auto i:nums){
+            int t=i-rev(i);
+            mp[(t)]++;
+            diff.push_back(t); 
+        }
+
+        for(int i=0; i<n; i++){
+            if(mp[diff[i]]>1){
+                count = (count + mp[diff[i]]-1)%MOD;
+                mp[diff[i]]--;
+            }
+        }
+
+        return count;
+    }
+};
