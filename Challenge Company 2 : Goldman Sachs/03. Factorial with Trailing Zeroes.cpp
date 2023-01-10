32e37bc42e0f7b95846e9688722d47ed43428d2a
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-10 06:33:18 +0530

    Solved

--- Challenge Company 2 : Goldman Sachs/03. Factorial with Trailing Zeroes.cpp
+++ Challenge Company 2 : Goldman Sachs/03. Factorial with Trailing Zeroes.cpp
@@ -1,3 +1,17 @@
 /*
 https://leetcode.com/problems/factorial-trailing-zeroes/
 */
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans=0;
+        
+        while(n>1){
+            n/=5;
+            ans+=n;
+        }
+
+        return ans;
+    }
+};
