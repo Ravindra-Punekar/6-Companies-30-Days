31660a660230c8b7bc4bee0d4fab1ce9190de636
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-14 20:36:37 +0530

    Solved

--- Challenge Company 3 : Adobe/02. Increasing Triplet Sequence.cpp
+++ Challenge Company 3 : Adobe/02. Increasing Triplet Sequence.cpp
@@ -1,3 +1,15 @@
 /*
 https://leetcode.com/problems/increasing-triplet-subsequence/
 */
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n1 = INT_MAX, n2 = INT_MAX;
+        for(auto n3: nums)
+            if(n3<=n1) n1 = n3;
+            else if(n3<=n2) n2 = n3;
+            else return true;
+        return false;
+    }
+}; 
