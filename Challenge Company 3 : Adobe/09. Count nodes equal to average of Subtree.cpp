94e5d2beb4fc9b12dc4c2ea3ed2e1ca81411eef8
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-14 20:41:00 +0530

    Solved

--- Challenge Company 3 : Adobe/09. Count nodes equal to average of Subtree.cpp
+++ Challenge Company 3 : Adobe/09. Count nodes equal to average of Subtree.cpp
@@ -1,3 +1,25 @@
 /*
 https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/
 */
+
+class Solution {
+public:
+    int c=0;
+    pair<int,int> tSum(TreeNode* root){
+        if(root==NULL) return {0,0};
+        
+        auto leftTreeeSum = tSum(root->left);
+        auto rightTreeSum = tSum(root->right);
+
+        int n = 1 + leftTreeeSum.second + rightTreeSum.second;      // no. of nodes
+        int sum = root->val+ leftTreeeSum.first+ rightTreeSum.first;  // sum
+        c += (root->val == sum/n);              // avg count
+        
+        return {sum, n};
+    }
+    
+    int averageOfSubtree(TreeNode* root) {
+        tSum(root);
+        return c;
+    }
+};
