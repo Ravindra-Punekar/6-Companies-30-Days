bffffbb056a04eb23544d0d4e299d29727648c42
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-16 22:57:18 +0530

    Solved

--- Challenge Company 2 : Goldman Sachs/10. People and Secret.cpp
+++ Challenge Company 2 : Goldman Sachs/10. People and Secret.cpp
@@ -1,3 +1,30 @@
 /*
 https://leetcode.com/problems/number-of-people-aware-of-a-secret/
 */
+
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        int MOD = 1e9+7;
+        vector<long long> dp(n+1,0);   
+        dp[0] = 0;
+        for(int i=1; i<=delay; i++)
+            dp[i] = 1;   
+        
+        for(int i = delay+1; i<=n; i++){
+            for(int j = delay; j<forget; j++){    
+                if(i-j<=0)
+                    break;
+                dp[i] =(dp[i]%MOD + dp[i-j]%MOD)%MOD;
+            }
+            dp[i]+=1;
+            
+            if(i-forget>=1) dp[i]--;          
+        }
+
+        // for(auto i : dp)
+        //     cout<<i<<" ";
+
+        return dp[n]%MOD; 
+    }
+};
