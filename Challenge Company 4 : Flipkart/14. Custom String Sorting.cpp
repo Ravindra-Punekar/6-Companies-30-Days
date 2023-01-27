1f3e04baef25e6967bfc5ca3806bee8d0cf233ed
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-28 00:15:29 +0530

    Solved

--- Challenge Company 4 : Flipkart/14. Custom String Sorting.cpp
+++ Challenge Company 4 : Flipkart/14. Custom String Sorting.cpp
@@ -1,3 +1,26 @@
 /*
 https://leetcode.com/problems/custom-sort-string/
 */
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string ans;
+        unordered_map<char,int> mp;
+
+        for(auto ch:s)
+            mp[ch]++;
+
+        for(auto ch:order){
+            if(mp.count(ch)){
+                ans.insert(ans.end(),mp[ch], ch);
+                mp.erase(ch);
+            }
+        }
+
+        for(auto it:mp){
+            ans.insert(ans.end(),it.second, it.first);
+        }
+        return ans;
+    }
+};
