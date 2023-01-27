d073d4682b4725cda327cdfba2135cdc954c8531
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-28 00:17:50 +0530

    Solved

--- Challenge Company 4 : Flipkart/03. Remove Zero Sum Consecutive Nodes.cpp
+++ Challenge Company 4 : Flipkart/03. Remove Zero Sum Consecutive Nodes.cpp
@@ -1,3 +1,40 @@
 /*
 https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
 */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        ListNode* temp = new ListNode(0);    //head can also be deleted
+        temp->next = head;
+        ListNode* curr = temp;
+        
+        unordered_map<int, ListNode*> mp;
+        int pSum=0;
+        while(curr){
+            pSum += curr->val;
+            mp[pSum] = curr;  //if pSum appears >1 times, prev sublist get ignored 
+            curr = curr->next;
+        }
+        
+        pSum=0;
+        curr = temp;
+        while(curr){
+            pSum += curr->val;
+            curr->next = mp[pSum]->next;
+            curr = curr->next;
+        }
+        
+        return temp->next;
+    }
+};
