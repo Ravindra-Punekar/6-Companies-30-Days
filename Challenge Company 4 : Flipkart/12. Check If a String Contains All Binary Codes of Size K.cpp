7a58aae7d90060294f585484f0409ce738a71362
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-28 00:16:29 +0530

    Solved

--- Challenge Company 4 : Flipkart/12. Check If a String Contains All Binary Codes of Size K.cpp
+++ Challenge Company 4 : Flipkart/12. Check If a String Contains All Binary Codes of Size K.cpp
@@ -1,3 +1,23 @@
 /*
 https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
 */
+
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        if(k>s.size()) return false;
+        unordered_set<string> st;
+
+        string win = s.substr(0, k);
+        st.insert(win);
+        
+        for(int i=1; i<=s.size()-k; i++){
+            win.erase(win.begin());
+            win+= s[i+k-1];
+            // cout<<win<<" ";
+            st.insert(win);
+        }
+        // for(auto str:st) cout<<str<<" ";
+        return st.size()==pow(2,k);
+    }
+};
