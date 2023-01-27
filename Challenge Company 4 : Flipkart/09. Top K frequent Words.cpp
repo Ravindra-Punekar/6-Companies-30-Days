eacc0d614f7738c6baf8cdf3ef6f14db4636dc28
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-28 00:16:03 +0530

    Solved

--- Challenge Company 4 : Flipkart/09. Top K frequent Words.cpp
+++ Challenge Company 4 : Flipkart/09. Top K frequent Words.cpp
@@ -1,3 +1,30 @@
 /*
 https://leetcode.com/problems/top-k-frequent-words/
 */
+
+class Solution {
+public:
+    static bool cmp(pair<string, int>& a, pair<string, int>& b){
+        return (a.second == b.second)? (a.first < b.first) : (a.second > b.second);
+    }
+ 
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<string> ans;
+        map<string , int> mp;
+
+        for(auto s:words)
+            mp[s]++;
+        
+        vector<pair<string, int>> temp;
+ 
+        for (auto& it : mp) 
+            temp.push_back(it);
+        
+        sort(temp.begin(), temp.end(), cmp);
+        
+        for(int i=0;i<k;i++)
+            ans.push_back({temp[i].first});
+        
+        return ans;
+    }
+};
