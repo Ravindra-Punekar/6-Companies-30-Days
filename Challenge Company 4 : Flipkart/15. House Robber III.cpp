f27827c024c3de356b53b3ab2c0abae71e3b9077
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-30 23:03:30 +0530

    Solved

--- Challenge Company 4 : Flipkart/15. House Robber III.cpp
+++ Challenge Company 4 : Flipkart/15. House Robber III.cpp
@@ -1,3 +1,33 @@
 /*
 https://leetcode.com/problems/house-robber-iii/
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int,int> solve(TreeNode* root){    
+        if(!root) return {0,0};
+        auto left = solve(root->left);
+        auto right = solve(root->right);
+
+        int robRoot = root->val + left.second + right.second;
+        int dontRobRoot = max(left.first, left.second) +max(right.first, right.second) ;
+        // cout<<root->val<<" "<<robRoot<<" "<<dontRobRoot<<endl;
+        return {robRoot, dontRobRoot};
+    }
+
+    int rob(TreeNode* root) {
+        auto p = solve(root);
+        return max(p.first, p.second);
+    }
+};
