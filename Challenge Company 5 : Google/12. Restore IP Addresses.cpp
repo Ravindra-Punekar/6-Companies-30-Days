e4357abcc3a97329bde0431677cb6120117706a5
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-31 20:35:09 +0530

    Solved

--- Challenge Company 5 : Google/12. Restore IP Addresses.cpp
+++ Challenge Company 5 : Google/12. Restore IP Addresses.cpp
@@ -1,3 +1,31 @@
 /*
 https://leetcode.com/problems/restore-ip-addresses/
 */
+
+class Solution {
+public:
+    void solve(string& s, vector<string>& ans, string cur_str, string lastdig, int i, int dots ){
+       
+        if( stoi(lastdig)>255|| dots>3 || i>s.size())
+            return;
+        if(lastdig[0]=='0' && lastdig.size()>1)return;
+        
+        if(i==s.size()){
+            if(dots==3)
+                ans.push_back(cur_str);
+            return ;
+        }
+
+        string t="";
+        solve(s, ans, cur_str+"."+s[i], t+s[i], i+1, dots+1);   // take .
+
+        solve(s, ans, cur_str+s[i], lastdig+s[i], i+1, dots);   // skip .
+    }
+
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> ans;
+        string cur_str;
+        solve(s, ans, cur_str+s[0], cur_str+s[0], 1, 0);
+        return ans;
+    }
+};
