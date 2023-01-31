ea4ca1b284bb6a3a376e7b27ee83687ac96db3ef
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-02-01 04:26:17 +0530

    Solved

--- Challenge Company 5 : Google/09. Maximum Numbers of Coins you can get.cpp
+++ Challenge Company 5 : Google/09. Maximum Numbers of Coins you can get.cpp
@@ -1,3 +1,22 @@
 /*
 https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
 */
+
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        int coins =0;
+        sort(piles.begin(), piles.end());
+        
+        int k=piles.size()/3;
+        
+        int i=piles.size()-2;
+        
+        while(k--){
+            coins += piles[i];
+            i-=2;
+        }
+
+        return coins;
+    }
+};
