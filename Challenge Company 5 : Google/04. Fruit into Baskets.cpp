450a4f881e2662ec08d9514da449b89ad913f42d
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-01-31 20:35:58 +0530

    Solved

--- Challenge Company 5 : Google/04. Fruit into Baskets.cpp
+++ Challenge Company 5 : Google/04. Fruit into Baskets.cpp
@@ -1,3 +1,21 @@
 /*
 https://leetcode.com/problems/fruit-into-baskets/
 */
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        
+        unordered_map<int, int> count;
+        int i, j;
+        for (i = 0, j = 0; j < fruits.size(); ++j) {
+            count[fruits[j]]++;
+            if (count.size() > 2) {
+                if (--count[fruits[i]] == 0)count.erase(fruits[i]);
+                i++;
+            }
+        }
+        return j - i;
+        
+    }   
+};
