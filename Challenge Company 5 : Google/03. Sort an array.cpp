a4c5cf5bc2d5d34204c5f1ad15e93bcd4fae4601
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2023-02-02 01:09:17 +0530

    Solved

--- Challenge Company 5 : Google/03. Sort an array.cpp
+++ Challenge Company 5 : Google/03. Sort an array.cpp
@@ -1,3 +1,46 @@
 /*
 https://leetcode.com/problems/sort-an-array/
 */
+
+class Solution {
+public:
+    void merge(vector<int>& nums, int s, int mid, int e){
+        vector<int> merged(e-s+1, 0);
+
+        int ind1 = s;
+        int ind2 = mid+1;
+        int k=0;
+
+        while(ind1<=mid && ind2<= e){
+            if(nums[ind1] <nums[ind2])
+                merged[k++] = nums[ind1++];
+            else
+                merged[k++] = nums[ind2++];
+        }
+
+        while(ind1<=mid)
+            merged[k++] = nums[ind1++];
+        
+        while(ind2<=e)
+            merged[k++] = nums[ind2++];
+        
+        for(int i=0; i<merged.size(); i++)
+            nums[i+s] = merged[i];
+    }
+
+    void mergeSort(vector<int>& nums, int s, int e){
+        if(s>=e) return;
+        int mid = s + (e-s)/2;
+
+        mergeSort(nums, s, mid);
+        mergeSort(nums, mid+1, e);
+
+        merge(nums, s, mid,  e);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        mergeSort(nums, 0, n-1);
+        return nums;
+    }
+};
